eb20df9a3c67ca259af0b628f41d00880f77a72c
aratoval <chy.pawel@gmail.com>
2018-11-19 14:14:20 +0000

    restart

--- project/serializers.py
+++
@@ -1,8 +0,0 @@
-from .models import Pressures
-from rest_framework import serializers
-
-
-class PressureSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Pressures
-        fields = ("id", "sensor_name", "date_time", "pressure", "temperature")
--- project/static/js/my/zoom_img.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-     const element = document.querySelectorAll("img.image-base");
-     element.forEach(e => e.addEventListener("mouseover"), function (event) {
-         event.
-     });
-});
