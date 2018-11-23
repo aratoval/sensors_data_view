8d4c05a97af8250990b8002604f6398c64deedc5
aratoval <chy.pawel@gmail.com>
2018-11-23 11:51:34 +0000

    strona w budowie

--- sensors_data_view/urls.py
+++ sensors_data_view/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from data_view.views import under_construction
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', under_construction)
 ]
